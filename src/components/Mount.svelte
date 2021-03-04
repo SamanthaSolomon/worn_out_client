61d056e2ec0073fce19798a7f84811a7eecc0bff
Samantha Solomon <sleahsolomon@gmail.com>
2021-03-04 14:38:09 -0500

    built closet component

--- src/components/Mount.svelte
+++ src/components/Mount.svelte
@@ -17,6 +17,16 @@ onMount( async () => {
 
 <div>
     {#each items as item}
-    <p>{item.category}</p>
+    <img src={item.img} alt={item.color}{item.style}>
+    <h4>{item.style} | {item.color}</h4>
+    <p>{item.use} | {item.category}</p>
+    <h3>Worn {item.wear_count} times since {item.created_at}</h3>
+    
     {/each}
 </div>
+
+<style>
+   img {
+    width: 10rem
+   } 
+</style>
