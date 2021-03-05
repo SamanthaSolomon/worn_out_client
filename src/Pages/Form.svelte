72aca7583387a38e61b3583907f7a824545bbf12
Samantha Solomon <sleahsolomon@gmail.com>
2021-03-05 17:56:09 -0500

    built form

--- src/Pages/Form.svelte
+++ src/Pages/Form.svelte
@@ -7,8 +7,6 @@
     import Header from '../components/Header.svelte'
 
     //form
-    import { createForm } from "svelte-forms-lib";
-
     const { form, handleChange, handleSubmit } = createForm({
       initialValues: {
         title: "",
@@ -26,42 +24,110 @@
 <div class="form">
     <Header />
     <p>Add new item to my closet</p>
-    <form on:submit={handleSubmit}>
-    <label for="title">title</label>
+
+    <form class="form-inputs" on:submit={handleSubmit}>
+        <label for="img">Image (url)</label>
+        <input id="img" 
+        name="img"
+        on:change={handleChange}
+        bind:value={$form.img}
+        />
+
+    <label for="category">Category</label>
     <select
-      id="title"
-      name="title"
+      id="category"
+      name="category"
       on:change={handleChange}
-      bind:value={$form.title}>
-      <option></option>
-      <option>Mr.</option>
-      <option>Mrs.</option>
-      <option>Mx.</option>
+      bind:value={$form.category}>
+      <option>Top</option>
+      <option>Bottom</option>
+      <option>Shoes</option>
+      <option>Onepeice</option>
+      <option>Accessory</option>
+      <option>Outerwear</option>
     </select>
 
-    <label for="name">name</label>
-    <input
-      id="name"
-      name="name"
+    <label for="style">Style</label>
+    <select
+      id="style"
+      name="style"
       on:change={handleChange}
-      bind:value={$form.name}
-    />
+      bind:value={$form.style}>
+      <option>Sweater</option>
+      <option>Sweatshirt</option>
+      <option>T-shirt</option>
+      <option>Tank-top</option>
+      <option>Button-up</option>
+      <option>Long-sleaved</option>
+      <option>Pants</option>
+      <option>Leggings</option>
+      <option>Sweatpants</option>
+      <option>Shorts</option>
+      <option>Skirt</option>
+      <option>Dress</option>
+      <option>Jumpsuit</option>
+      <option>Overcoat</option>
+      <option>Jacket</option>
+      <option>Sneaker</option>
+      <option>Bag</option>
+      <option>Heels</option>
+    </select>
 
-    <label for="email">email</label>
-    <input
-      id="email"
-      name="email"
+    <label for="use">Use</label>
+    <select
+      id="use"
+      name="use"
       on:change={handleChange}
-      bind:value={$form.email}
-    />
+      bind:value={$form.use}>
+      <option>Everday</option>
+      <option>Work</option>
+      <option>Specialty</option>
+      <option>Workout</option>
+      <option>Outdoor</option>
+      <option>Sleep/lounge</option>
+    </select>
 
-    <button type="submit">Submit</button>
+    <label for="style">Color</label>
+    <select
+      id="color"
+      name="color"
+      on:change={handleChange}
+      bind:value={$form.color}>
+      <option>Blue</option>
+      <option>Black</option>
+      <option>Grey</option>
+      <option>Cream</option>
+      <option>White</option>
+      <option>Pink</option>
+      <option>Green</option>
+      <option>Yellow</option>
+      <option>Orange</option>
+      <option>Brown</option>
+      <option>Denim</option>
+      <option>Metalic</option>
+      <option>Multi</option>
+      <option>Purple</option>
+      <option>Orange</option>
+      <option>Red</option>
+    </select>
   </form>
+
     <AddUpdateButton/>
 </div>
 
 <style>
     .form{
         text-align: center;
+    }
+
+    .form-inputs{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    input, select{
+        width: 20%;
+        margin-bottom: 1rem
     }
 </style>
