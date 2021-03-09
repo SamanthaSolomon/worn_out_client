4cbed5f797b23359e8008e67ceeb0df0584991ee
Samantha Solomon <sleahsolomon@gmail.com>
2021-03-09 12:21:36 -0500

    added item id and else block to closet component

--- src/Pages/Closet.svelte
+++ src/Pages/Closet.svelte
@@ -6,13 +6,13 @@ import { onMount } from "svelte"
 import Header from "../components/Header.svelte"
 
 //api call for all items
-let items = []
+// let items = []
 
-onMount( async () => {
-    const response = await fetch('https://damp-peak-94577.herokuapp.com/users/1/items')
-    items = await response.json()
+// onMount( async () => {
+//     const response = await fetch('https://damp-peak-94577.herokuapp.com/users/1/items')
+//     items = await response.json()
 
-})
+// })
 
 
 </script>
@@ -20,12 +20,13 @@ onMount( async () => {
 
 <div>
     <Header />
-    {#each items as item}
+    {#each items as item (item.id)}
     <img src={item.img} alt={item.color}{item.style}>
     <a href='/item'><h4>{item.style} | {item.color}</h4></a>
     <p>{item.use} | {item.category}</p>
     <h3>Worn {item.wear_count} times since {item.created_at}</h3>
-    
+    {:else}
+    <h3>There are no items in your closet.</h3>
     {/each}
 </div>
 
